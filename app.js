97a6578e66dcac6943e325b80c6475ac4d0b4794
Vonight1 <hsare555thpsail@gmail.com>
2025-02-24 16:32:02 +0700

    add tempage

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const chalk = require('chalk'); // If using chalk@4, this works
 const debug = require('debug')('app');
 const morgan = require('morgan'); // Added morgan
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req, res) => {
-    res.send('index',{username: 'Praguy554', customers: ["kit1112","kitto",]});
+    res.send('Hello World');
 });
 
 app.listen(port, () => {
